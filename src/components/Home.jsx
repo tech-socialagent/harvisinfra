979b8f8d29e721961ce92d8a183e0d143ca7f81a
bkmahapatra <bkmahapatra27@gmail.com>
2023-03-01 15:57:55 +0530

    home base complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "../Styles/Home.css";
+import BannerVdo from "../assets/Harvis video.mp4";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home_section">
+      <video autoPlay loop muted id="vdo_file" className="banner_vdo">
+        <source src={BannerVdo} type="video/mp4" className="vdo" />
+      </video>
+      <div className="banner_title">
+        <h2 className="home_title">You Dream It. We Build It.</h2>
+        <div className="main_btn1">Know More</div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
