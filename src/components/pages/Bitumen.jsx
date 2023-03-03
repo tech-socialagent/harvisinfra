10fffac42245b7dc3f9b835e4c0548434e0f83e6
bkmahapatra <bkmahapatra27@gmail.com>
2023-03-03 12:00:49 +0530

    bitumen done

--- src/components/pages/Bitumen.jsx
+++ src/components/pages/Bitumen.jsx
@@ -3,43 +3,212 @@ import "../../Styles/ProductPage.css";
 
 const Bitumen = () => {
   return (
-    <div className="product_page">
-      <div className="flo_art2"></div>
-      <div className="section_header">
-        <h3>Bitumen.</h3>
-      </div>
-
-      <div className="page_section_body">
-        <div className="page_product_desc">
-          <p>
-            Bitumen is a black, sticky, and highly viscous material that is a
-            naturally occurring substance. It is primarily composed of
-            hydrocarbons and is found in petroleum deposits.It is a key
-            component in the development of infrastructure projects,
-            particularly in the construction of roads and highways. Bitumen is a
-            dense, viscous material derived from petroleum that is used to bind
-            and protect the aggregate (such as gravel or crushed stone) in
-            asphalt pavements.
-          </p>
-          <p>
-            Infrastructure project development firms use bitumen for its
-            durability, flexibility, and ability to withstand harsh weather
-            conditions, making it ideal for use in long-term construction
-            projects. Bitumen is also commonly used to waterproof structures,
-            such as bridges and culverts, to prevent water from penetrating the
-            surface and causing damage over time.
-          </p>
-          <p>
-            The use of bitumen in infrastructure projects is a critical factor
-            in ensuring that the infrastructure is safe, efficient, and
-            long-lasting. In addition, infrastructure project development firms
-            typically invest in high-quality bitumen products to ensure
-            consistent and reliable performance, as well as to minimize the
-            likelihood of premature failure.
-          </p>
+    <div className="bitumen_section">
+      <div className="product_page">
+        <div className="flo_art2"></div>
+        <div className="section_header">
+          <h3>Bitumen.</h3>
         </div>
-        <div className="product_slide">
 
+        <div className="page_section_body">
+          <div className="page_product_desc">
+            <p>
+              Bitumen is a black, sticky, and highly viscous material that is a
+              naturally occurring substance. It is primarily composed of
+              hydrocarbons and is found in petroleum deposits.It is a key
+              component in the development of infrastructure projects,
+              particularly in the construction of roads and highways. Bitumen is
+              a dense, viscous material derived from petroleum that is used to
+              bind and protect the aggregate (such as gravel or crushed stone)
+              in asphalt pavements.
+            </p>
+            <p>
+              Infrastructure project development firms use bitumen for its
+              durability, flexibility, and ability to withstand harsh weather
+              conditions, making it ideal for use in long-term construction
+              projects. Bitumen is also commonly used to waterproof structures,
+              such as bridges and culverts, to prevent water from penetrating
+              the surface and causing damage over time.
+            </p>
+            <p>
+              The use of bitumen in infrastructure projects is a critical factor
+              in ensuring that the infrastructure is safe, efficient, and
+              long-lasting. In addition, infrastructure project development
+              firms typically invest in high-quality bitumen products to ensure
+              consistent and reliable performance, as well as to minimize the
+              likelihood of premature failure.
+            </p>
+          </div>
+          <div className="product_slide"></div>
+        </div>
+      </div>
+      <div className="bitumen_moreinfo">
+        {/* <div className="pro_vg30">
+          <div className="vg30_desc">
+            <div className="pro_slider"></div>
+            <div className="pro_info"></div>
+          </div>
+          <div className="char_vg30"></div>
+        </div> */}
+
+        <div className="pro_vg30">
+          <div className="vg30_desc">
+            <div className="pro_slider">
+              <img
+                src={require("../../assets/photos/bitumen.jpeg")}
+                alt=""
+                className="proimg"
+              />
+            </div>
+            <div className="pro_info">
+              <div className="item_heading">
+                <h4>VG 30 Bitumen.</h4>
+              </div>
+              <p>
+                VG-30 is generally utilised in the construction of exceptionally
+                industrial grade Bitumen Pavements that must withstand heavy
+                traffic loads. It can be substituted with Penetration Grade
+                60/70.
+              </p>
+              <p>
+                In India, VG 30 bitumen is a common bitumen grade. This type of
+                bitumen is preferred by Indian importers since its performance
+                in Indian road building has been proven. In India, VG 30 grade
+                bitumen is also utilized for plastic roadways.
+              </p>
+              <p>
+                We deploy VG30 in high-temperature settings because of its
+                strong thermal sensitivity. Instead of bitumen penetration
+                grades, VG-30 bitumen is also suited for usage in hot and wet
+                weather. The higher the viscosity of the bitumen, the less
+                probable it is to be affected by water.
+              </p>
+              <h5>
+                What are the Advantages of Using Viscosity Grade 30 Bitumen?
+              </h5>
+              <ul>
+                <li>It is appropriate for temperatures exceeding 30 °C.</li>
+                <li>
+                  Bitumen with a higher viscosity is more suited for clayed
+                  soil.
+                </li>
+                <li>
+                  In wet conditions, VG30 is more suited for road building.
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="char_vg30">
+            {/* <img src={require("../../assets/Group.png")} alt="" className="vector1" /> */}
+            <div className="char_title">Characteristics.</div>
+            <table>
+              <tr>
+                <td>Absolute Viscosity 60 °C, Poises, min</td>
+                <td>2400-3600</td>
+              </tr>
+              <tr>
+                <td>Kinematic Viscosity, 135 C, CST, min</td>
+                <td>350</td>
+              </tr>
+              <tr>
+                <td>Flash point, C, min</td>
+                <td>220</td>
+              </tr>
+              <tr>
+                <td>Solubility in trichloroethylene, %, min</td>
+                <td>99.0</td>
+              </tr>
+              <tr>
+                <td>Penetration Value of Vg30, at 25°C</td>
+                <td>45</td>
+              </tr>
+              <tr>
+                <td>Softening point, "C, min</td>
+                <td>47</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="pro_vg30 pro_vg40">
+          <div className="vg30_desc">
+            <div className="pro_slider">
+              <img
+                src={require("../../assets/photos/bitumen.jpeg")}
+                alt=""
+                className="proimg"
+              />
+            </div>
+            <div className="pro_info">
+              <div className="item_heading">
+                <h4>VG 40 Bitumen.</h4>
+              </div>
+              <p>
+                VG-40 is used in highly stressed areas such as intersections,
+                near toll booths and truck parking lots in lieu of old 30/40
+                Penetration Grade. Due to its higher Viscosity, stiffer Bitumen
+                Mixes can be produced to improve resistance to shoving and other
+                problems associated with higher temperature and heavy traffic
+                loads.
+              </p>
+              <p>
+                VG-40 bitumen is used in areas in which high pressure comes from
+                heavy traffic loads, such as intersections, near tolls booths,
+                and truck parking lots.Due to the high viscosity of this
+                bitumen, it is more appropriate for improving resistance to
+                shoving and other problems associated with higher temperatures
+                and heavy traffic loads.
+              </p>
+              <p>
+                It's long time that industries use vg-40 instead of the
+                penetration grade bitumen 40/50.The difference between VG30 and
+                Vg40The difference between VG-30 and VG-40 is that the VG 30
+                bitumen is suitable for mild weather and VG 40 bitumen is better
+                to use in hot climate. If you need to consult before buying vg40
+                contact us.
+              </p>
+              <h5>
+                What are the Advantages of Using Viscosity Grade 40 Bitumen?
+              </h5>
+              <ul>
+                <li>It is suitable for a temperature above 40 C</li>
+                <li>Its absolute viscosity is 3200</li>
+                <li>
+                  It is a preferred type of bitumen when heavy traffic load is
+                  expected
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="char_vg30">
+            {/* <img src={require("../../assets/Group.png")} alt="" className="vector1" /> */}
+            <div className="char_title">Characteristics.</div>
+            <table>
+              <tr>
+                <td>Absolute Viscosity 60 °C, Poises, min</td>
+                <td>2400-3600</td>
+              </tr>
+              <tr>
+                <td>Kinematic Viscosity, 135 C, CST, min</td>
+                <td>350</td>
+              </tr>
+              <tr>
+                <td>Flash point, C, min</td>
+                <td>220</td>
+              </tr>
+              <tr>
+                <td>Solubility in trichloroethylene, %, min</td>
+                <td>99.0</td>
+              </tr>
+              <tr>
+                <td>Penetration Value of Vg30, at 25°C</td>
+                <td>45</td>
+              </tr>
+              <tr>
+                <td>Softening point, "C, min</td>
+                <td>47</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     </div>
